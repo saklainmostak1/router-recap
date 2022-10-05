7bf41be48068c2c22f34aabf7e490914fb0fc203
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-06 05:44:32 +0600

    push

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Friend.css'
 
 const Friend = ({friend}) => {
-    const {name, email, username} = friend;
+    const {name, email, username, id} = friend;
     return (
         <div className='friend'>
             <h3>Name: {name} </h3>
             <p>Email: {email} </p>
-            <p>UserName: {username} </p>
+            <p>UserName: <Link to={`/friend/${id}`}> {username} </Link> </p>
         </div>
     );
 };
