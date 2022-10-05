8bcd1f62c1ef315578e9c59f7fd374db01929806
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-06 05:21:40 +0600

    push

---
+++ src/components/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Products = () => {
+    return (
+        <div>
+            <h3>This is products page</h3>
+        </div>
+    );
+};
+
+export default Products;
